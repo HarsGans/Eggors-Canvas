c3dca4b9c24ece494e348c5538638eab7039f165
Eggor <fanshorrible4@gmail.com>
2025-09-05 21:15:22 +0700

    use simple javascript lah cape wa bjier

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const myHeaders = new Headers();
 myHeaders.append("x-api-key", "fwjFlNJj2Ei7625vMk9j1zpggqIrBxkbxrmwa4fEKngIkqiHZXlKaGJHY2lPaUpTVXpJMU5pSXNJbXRwWkNJNkluTnBaeTB5TURJeExUQTNMVEV6VkRFNE9qVXhPalE1V2lJc0luUjVjQ0k2SWtwWFZDSjkuZXlKaVlYTmxRWEJwUzJWNUlqb2labmRxUm14T1Ntb3lSV2szTmpJMWRrMXJPV294ZW5CblozRkpja0o0YTJKNGNtMTNZVFJtUlV0dVowbHJjV2xJSWl3aWIzZHVaWEpKWkNJNklqTXdNVEEwT0RJMU9EQWlMQ0poZFdRaU9pSlNiMkpzYjNoSmJuUmxjbTVoYkNJc0ltbHpjeUk2SWtOc2IzVmtRWFYwYUdWdWRHbGpZWFJwYjI1VFpYSjJhV05sSWl3aVpYaHdJam94TnpVMU56Y3hPREl3TENKcFlYUWlPakUzTlRVM05qZ3lNakFzSW01aVppSTZNVGMxTlRjMk9ESXlNSDAuS2tfV3k5M01YWV9xWU8yWmxwelRSbjE3ekVUTENDcUoxVk40SHd1X0hWT09VZWY2S1k2WUZHbjhVZVkyalpvZzZjRzVuM2hfYWFzSWZTU19Rb1E3bE1hMF9PeFE2NmZjMHB0UkN3TkRpbUx1cHdlQlUwNlBlQnRkYjlqSkVpZ3I5VVRaZmlyNnR2Q2JZR2p4aTBoTXVFU0VBb3dOYWVLQzRxTDMxZkQ4cW91T3pLTnpPNVNqVEF6VjA0SW9tbVl3NXRmZjBJMkpFUlhObFlzTlNhRGdpSTMySEYxNnVsSl84dnljMGtBUFc3WmFQbnhzRGFKaThtampLb0E2Z09LRUFQMDAtbVFRUkNIMDhhV0JEVHE5dlhuQ3ZDVjFSbTdhcmlXUjdjN2I0c284UWtmdnVVaWx1dHhUSjc1MXFLSkxmWU9BOHdYbW54UjRCdkIycHNYRDJn");
-myHeaders.append("Content-type", "application/json");
 
 const placeholder = "Media/sybau.jpg"
 
@@ -12,19 +11,16 @@ const requestOptions = {
 
 fetch("https://apis.roblox.com/cloud/v2/users/3010482580:generateThumbnail?size=150&shape=ROUND", requestOptions)
   .then((response) => response.json())
-  .then(data => {
-        const imageUrl= data.imageUrl;
-        document.getElementById("avatar").src = imageUrl;
-  })
-  .catch((error) => console.error(error));
 
-  const img = satuan.querySelector("img.picture-profile");
 
-  if(img) {
-    img.src = placeholder;
+  ;
 
-    img.onerror = function() { this.onerror = null; this.src = placeholder; };
-    img.width = 150;
-    img.height = 150;
-  }
+  //img = document.querySelector("img.profile-picture");
 
+  //if(img) {
+    //img.src = placeholder;
+
+    //img.onerror = function() { this.onerror = null; this.src = placeholder; };
+    //img.width = 150;
+    //img.height = 150;
+  //};
